4ccf5b269c12b2a0245fafff60a7e54855579564
tronross <john_ross@analogforever.ca>
2023-04-03 21:35:25 -0400

    Add basic component framework

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+//////////////////////
+// Button Component
+//////////////////////
+
+// Stylesheet
+
+// Component
+
+export default function Button(props) {
+
+
+  return (
+    <button>
+      
+    </button>
+
+  )
+};
