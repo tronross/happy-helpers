686a904d84787f1ca0d9b34879710380df2bc6ff
ACF <alricfernandes11@gmail.com>
2023-04-05 17:52:46 -0400

    Adding form inputs to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,22 @@
 export default function Sidebar(props) {
   return (
-    <div className="bg-teal-600 w-[1200px] h-screen sticky top-0">Filters</div>
+    <>
+    <div className="bg-teal-600 w-[1200px] h-screen sticky top-0">Filters
+    <form action="/api/tasks" method="get">
+        <input type="text" placeholder="Location"/>
+        <select>
+          <option value="all-tasks">All Tasks</option>
+          <option value="errands">Errands</option>
+          <option value="house-work">House Work</option>
+          <option value="personal-care">Personal Care</option>
+          <option value="tech-support">Tech Support</option>
+          <option value="charity-causes">Charity & Causes</option>
+        </select>
+        <input type="date" placeholder="Date"/>
+        <input type="submit" value="Submit" />
+    </form>
+    
+    </div>
+    </>
   )
 }
