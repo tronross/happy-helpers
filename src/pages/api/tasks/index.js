aa4d3f8c41f232225517cf344cd46b19e7ffd24b
tronross <john_ross@analogforever.ca>
2023-04-06 16:01:07 -0400

    Fix api/tasks route handler

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,13 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 export async function getTasks() {
-  const prisma = new PrismaClient();
+  
   const tasks = await prisma.task.findMany()
   return tasks;
 }
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
+ if (req.method === 'GET') {
   const tasks = await prisma.task.findMany()
-  res.json(tasks)
+  console.log(tasks)
+  res.json({tasks})
+ } if (req.method === 'POST') {
+  console.log(req.body)
+  res.send('hello')
+ }
 }
