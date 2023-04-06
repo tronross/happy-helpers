529d02f9ec95193a93b1f3c848479f0ceafefc7d
tronross <john_ross@analogforever.ca>
2023-04-06 15:24:56 -0400

    Add basic props implementation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,25 @@
 export default function Sidebar(props) {
+  const dropdownSelect = (e) => {
+    const select = e.target.value
+    console.log(select)
+    props.setSelectedSidebar(select)
+  }
   return (
     <>
     <div className="bg-teal-600 w-[1200px] h-screen sticky top-0">Filters
     <form action="/api/tasks" method="get">
         <input type="text" placeholder="Location"/>
-        <select>
-          <option value="all-tasks">All Tasks</option>
+        <select onChange={dropdownSelect}>
+
+          {/* <option value="all-tasks">All Tasks</option>
           <option value="errands">Errands</option>
           <option value="house-work">House Work</option>
           <option value="personal-care">Personal Care</option>
           <option value="tech-support">Tech Support</option>
-          <option value="charity-causes">Charity & Causes</option>
+          <option value="charity-causes">Charity & Causes</option> */}
+          {props.sidebarOptions.map((item, idx) => {
+            return <option key={idx} value={item}>{item}</option>
+          })}
         </select>
         <input type="date" placeholder="Date"/>
         <input type="submit" value="Submit" />
