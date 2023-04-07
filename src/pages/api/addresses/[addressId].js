fa442a7621bfc35be075d1defaeae000f8710ec6
tronross <john_ross@analogforever.ca>
2023-04-06 23:42:10 -0400

    Add api GET PUT addresses/[addressId]

---
+++ src/pages/api/addresses/[addressId].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  if (req.method === 'GET') {
+    const { addressId } = req.query;
+    const address = await prisma.organization.findUnique({
+      where: {
+        id: parseInt(addressId)
+      }
+    })
+    console.log(address)
+    res.json({ address })
+  } else if (req.method === 'PUT') {
+    console.log(req.body)
+    const { addressId } = req.query;
+    res.send(`PUT api/users/${addressId}`)
+  }
+}
