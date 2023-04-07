09766db973bca5156dbcd63b6a4b60eacd7ad943
tronross <john_ross@analogforever.ca>
2023-04-06 23:41:04 -0400

    Fix variable names

--- src/pages/api/organizations/[organizationId].js
+++ src/pages/api/organizations/[organizationId].js
@@ -4,13 +4,13 @@ export default async function handler(req, res) {
   const prisma = new PrismaClient();
   if (req.method === 'GET') {
     const { organizationId } = req.query;
-    const task = await prisma.organization.findUnique({
+    const organization = await prisma.organization.findUnique({
       where: {
         id: parseInt(organizationId)
       }
     })
-    console.log(task)
-    res.json({ task })
+    console.log(organization)
+    res.json({ organization })
   } else if (req.method === 'PUT') {
     console.log(req.body)
     const { organizationId } = req.query;
