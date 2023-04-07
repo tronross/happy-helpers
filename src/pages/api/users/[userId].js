d3d6c20325a821237dd99f0ceb31c28ae30ef306
tronross <john_ross@analogforever.ca>
2023-04-06 22:48:54 -0400

    Add api POST users; add api GET and PUT users/[:userId]

---
+++ src/pages/api/users/[userId].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+ if (req.method === 'GET') {
+  const { userId } = req.query;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(userId)
+    }
+  })
+  console.log(user)
+  res.json({user})
+ } if (req.method === 'PUT') {
+  console.log(req.body)
+  const { userId } = req.query;
+  res.send(`api/users/${userId}`)
+ }
+}
---
+++ src/pages/api/users/index.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+ if (req.method === 'POST') {
+  console.log(req.body)
+  res.send('POST api/users')
+ }
+}
