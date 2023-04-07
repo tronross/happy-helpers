6f96b5fbd95ff9759f06d46ca3f6830017c1a7a3
tronross <john_ross@analogforever.ca>
2023-04-06 23:22:18 -0400

    Fix res.send messages

--- src/pages/api/users/[userId].js
+++ src/pages/api/users/[userId].js
@@ -2,18 +2,18 @@ import { PrismaClient } from '@prisma/client';
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
- if (req.method === 'GET') {
-  const { userId } = req.query;
-  const user = await prisma.user.findUnique({
-    where: {
-      id: parseInt(userId)
-    }
-  })
-  console.log(user)
-  res.json({user})
- } if (req.method === 'PUT') {
-  console.log(req.body)
-  const { userId } = req.query;
-  res.send(`api/users/${userId}`)
- }
+  if (req.method === 'GET') {
+    const { userId } = req.query;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(userId)
+      }
+    })
+    console.log(user)
+    res.json({ user })
+  } if (req.method === 'PUT') {
+    console.log(req.body)
+    const { userId } = req.query;
+    res.send(`PUT api/users/${userId}`)
+  }
 }
