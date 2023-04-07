a0b8b596b8835302407cde834a40aacab87087d6
tronross <john_ross@analogforever.ca>
2023-04-06 22:28:23 -0400

    Add initial API routes (GET, PUT) for tasks[:id]

---
+++ src/pages/api/tasks/[taskId].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+ if (req.method === 'GET') {
+  const { taskId } = req.query;
+  const task = await prisma.task.findUnique({
+    where: {
+      id: parseInt(taskId)
+    }
+  })
+  console.log(task)
+  res.json({task})
+ } if (req.method === 'PUT') {
+  console.log(req.body)
+  const taskId = (req.params['id']);
+  res.send(`api/task/${taskId}`)
+ }
+}
