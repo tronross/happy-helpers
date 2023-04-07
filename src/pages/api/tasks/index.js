50d3bc8bae1796153ff2b086726c910b574fe983
tronross <john_ross@analogforever.ca>
2023-04-06 23:10:38 -0400

    Lint; fix formatting

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -2,12 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
- if (req.method === 'GET') {
-  const tasks = await prisma.task.findMany()
-  console.log(tasks)
-  res.json({tasks})
- } else if (req.method === 'POST') {
-  console.log(req.body)
-  res.send('POST api/tasks')
- }
+  if (req.method === 'GET') {
+    const tasks = await prisma.task.findMany()
+    console.log(tasks)
+    res.json({ tasks })
+  } else if (req.method === 'POST') {
+    console.log(req.body)
+    res.send('POST api/tasks')
+  }
 }
