84f7f158cd9a68833950be288d6ee22d5359abfe
tronross <john_ross@analogforever.ca>
2023-04-06 23:16:16 -0400

    Add api GET organizations/[OrganizationId]

---
+++ src/pages/api/organizations/[organizationId].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  if (req.method === 'GET') {
+    const { organizationId } = req.query;
+    const task = await prisma.organization.findUnique({
+      where: {
+        id: parseInt(organizationId)
+      }
+    })
+    console.log(task)
+    res.json({ task })
+  } else if (req.method === 'PUT') {
+    console.log(req.body)
+    const { organizationId } = req.query;
+    res.send(`api/users/${organizationId}`)
+  }
+}
