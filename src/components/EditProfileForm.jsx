44669a15efc7761f93d07c754ae223148b511758
ACF <alricfernandes11@gmail.com>
2023-04-08 12:57:31 -0400

    Adding new component for edit profile form with initial setup

---
+++ src/components/EditProfileForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function EditProfileForm(EditProfileFormData, setEditProfileFormData) {
+
+  // TEMPLATE
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input />
+    </form>
+  );
+}
