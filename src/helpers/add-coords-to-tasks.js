c1324a3dd7655d97f156819350b338a6c43ee461
tronross <john_ross@analogforever.ca>
2023-04-10 18:03:52 -0400

    Remove dead code

--- src/helpers/add-coords-to-tasks.js
+++ src/helpers/add-coords-to-tasks.js
@@ -1,16 +1,5 @@
-// import axios from "axios";
-// import { PrismaClient } from "@prisma/client";
 
-
-
-export default function getAllTasks(tasks, addresses) {
-
-  // const prisma = new PrismaClient();
-  
-  // const tasks = await prisma.task.findMany()
-  // const addresses = await prisma.address.findMany()
-  
-  
+export default function addCoordsToTasks(tasks, addresses) {
     for (const task of tasks) {
       const taskAddress = addresses[task.addressId]
 
