fba932bd1f17d06e9d381bebb7a6c08f0bbf92e8
tronross <john_ross@analogforever.ca>
2023-04-10 22:08:19 -0400

    Add city to user in addCoordsToUser

--- src/helpers/add-coords-to-user.js
+++ src/helpers/add-coords-to-user.js
@@ -3,10 +3,12 @@ export default function addCoordsToUser(user, addresses) {
 
     const latitude = userAddress.latitude;
     const longitude = userAddress.longitude;
+    const city = userAddress.city
     console.log(latitude, longitude)
 
     user.latitude = `${latitude}`;
     user.longitude = `${longitude}`;
+    user.city = city;
 
     // console.log(user)
   }
