e98392d97dcbb7cbd4916ad25c3b15e3cb2f06ed
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-04-12 16:41:17 -0400

    Style logout button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,8 +8,8 @@
 
 export default function Button(props) {
 
-  const buttonStyle = `${props.buttonStyle} uppercase font-bold px-3 py-1 rounded text-white h-8 text-sm`;
-
+  const logout = props.buttonName === "LOGOUT" ? "bg-teal-600 hover:bg-teal-700": "";
+  const buttonStyle = `${logout} ${props.buttonStyle} uppercase font-bold px-3 py-1 rounded text-white h-8 text-sm`;
 
 
   return (
