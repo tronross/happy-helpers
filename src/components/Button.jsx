f308d88409d30c441112c01e2435ef0d6d6a9385
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-04-12 16:33:02 -0400

    Fix help button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,8 +11,9 @@ export default function Button(props) {
   const buttonStyle = `${props.buttonStyle} uppercase font-bold px-3 py-1 rounded text-white h-8 text-sm`;
 
 
+
   return (
-    <button className={buttonStyle} onClick={props.onClick} >
+    <button disabled={props.disabled} className={buttonStyle} onClick={props.onClick} >
       {props.buttonName}
     </button>
   )
