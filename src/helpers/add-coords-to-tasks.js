31b5f67366c71d0c5822da6512a72d135863487c
tronross <john_ross@analogforever.ca>
2023-04-12 13:34:05 -0400

    Fix addCoordsToTasks (pre-order array by id, offset index -1)

--- src/helpers/add-coords-to-tasks.js
+++ src/helpers/add-coords-to-tasks.js
@@ -1,17 +1,20 @@
 
 export default function addCoordsToTasks(tasks, addresses) {
+  tasks.sort((a, b) => a.id - b.id);
+  
   for (const task of tasks) {
-    const taskAddress = addresses[task.addressId]
-
+    const taskAddress = addresses[task.addressId - 1]
+    
     const latitude = taskAddress.latitude;
     const longitude = taskAddress.longitude;
     const city = taskAddress.city
     console.log(latitude, longitude, city)
-
+    
     task.latitude = `${latitude}`;
     task.longitude = `${longitude}`;
     task.city = city;
     // console.log(task)
+    console.log(`Task test: Task Name: ${task.name} ID: ${task.id} city ${task.city}`)
   }
 }
 
