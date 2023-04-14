a6b49ac95417cad486da7c66f8487a1bcbff2eeb
tronross <john_ross@analogforever.ca>
2023-04-13 22:37:37 -0400

    Add ternary op to abbreviate message in Header when All Categories

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,7 +2,7 @@ export default function PageHeader(props) {
   return (
     <div className="flex justify-between m-4 text-lg text-teal-600 font-bold mb-2">
       <section>
-        <h1 className="text-[1.5em]">Discover {props.category} Volunteer Opportunities in {props.city}</h1>
+        <h1 className="text-[1.5em]">Discover {props.category === "All Categories" ? '' : props.category} Volunteer Opportunities in {props.city}</h1>
       </section>
       <section className="flex">
         <p className="mx-3 hover:cursor-pointer" onClick={() => props.setView("List")}>LIST</p>
