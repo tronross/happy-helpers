ef071e175ee3b35d42c07fcc4f997efd3c2ec536
tronross <john_ross@analogforever.ca>
2023-04-14 14:57:10 -0400

    Fix add-distance-to-tasks (add ternary to accomodate different object syntax)

--- src/helpers/add-distance-to-tasks.js
+++ src/helpers/add-distance-to-tasks.js
@@ -2,12 +2,12 @@ import getDistance from './get-distance';
 
 export default function addDistanceToTasks(tasks, user) {
 
-  const userLat = user.address.latitude;
-  const userLong = user.address.longitude;
+  const userLat = user.latitude ? user.latitude : user.address.latitude;
+  const userLong = user.longitude ? user.longitude : user.address.longitude;
 
   for (const task of tasks) {
-    const taskLat = task.address.latitude;
-    const taskLong = task.address.longitude;
+    const taskLat = task.latitude ? task.latitude : task.address.latitude;
+    const taskLong = task.longitude ? task.longitude : task.address.longitude;
 
     const distance = getDistance(userLat, userLong, taskLat, taskLong);
 
