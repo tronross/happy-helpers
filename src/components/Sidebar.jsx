239084e0dafcce590c0c2eb36e04f9f46c66a5ab
tronross <john_ross@analogforever.ca>
2023-04-14 17:14:00 -0400

    Add distance filter buttons and function

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,11 +8,18 @@ export default function Sidebar(props) {
     props.setFilters(prev => ({...prev, category: select }));
   }
 
+  const closerThan = (e) => {
+    const distance = e.target.value
+    console.log(distance)
+    props.setFilters(prev => ({...prev, distance: distance }));
+  }
+
   const sortSelect = (e) => {
     const sort = e.target.value
     // console.log(sort)
     props.setFilters(prev => ({...prev, sort: sort }));
   }
+
 
   useEffect(() => {
     console.log(props.filters)
@@ -33,8 +40,19 @@ export default function Sidebar(props) {
         {props.sidebarOptions.map((item, idx) => {
           return <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={idx} value={item} onClick={dropdownSelect}>{item}</button>
         })}
-        <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={15} value={'All Categories'} onClick={dropdownSelect}>Reset Filter</button>
         </section>
+        <h5 className= "m-4 text-lg text-teal-600 font-bold mb-2">Distance:</h5>
+        <section class="inline-flex rounded-md shadow-sm">
+        {props.distances.map((distance, idx) => {
+          return  <button type="button" class="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-lg first:ml-0 last:rounded-r-lg border" key={idx} value={distance} onClick={closerThan} >
+          {distance}</button>
+        })}
+         
+    
+      </section>
+      <section className="max-w-xs flex flex-col rounded-md shadow-sm">
+        <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={15} value={'All Categories'} onClick={dropdownSelect}>Reset Filters</button>
+      </section>
     
       {/* <input type="date" placeholder="Date"/> */}
       {/* <input type="submit" value="Submit" /> */}
