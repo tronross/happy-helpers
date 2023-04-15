03916f6ed1b10d79f98ba6e4128bbb4d0f8299c2
tronross <john_ross@analogforever.ca>
2023-04-15 17:59:04 -0400

    Add styling for visual feedback

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,27 +14,27 @@ export default function NavBar(props) {
   return (
     <div className="flex items-center p-5 text-teal-600 uppercase tracking-wide text-gray-700 text-sm font-bold mb-2 fixed sticky top-0 z-50 bg-neutral-100">
       <ul className="flex w-1/2">
-        <li className='px-6 '>
-          Happy Helpers
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
+        <Link href="/home">Happy Helpers</Link>
         </li>
-        <li className='px-6 '>
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
           <Link href="/home">Home</Link>
         </li>
-        <li className='px-6 '>
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
           <Link href="/new-task">New</Link>
         </li>
       </ul>
       <ul className="flex items-center  w-1/2 justify-end">
-      <li className='px-6 '>
+      <li className='px-6 hover:text-teal-500 active:text-teal-700'>
           <Link href="/user-tasks">MyHelp</Link>
         </li>
-        <li className='px-6 '>
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
           <h2>Messages</h2>
         </li>
-        <li className='px-6 '>
-          <Link href="/my-profile/:id">{props.name}</Link>
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
+          <Link href={`/api/users/${props.id}`}>{props.name}</Link>
         </li>
-        <li className='px-6 '>
+        <li className='px-6 hover:text-teal-500 active:text-teal-700'>
           <Button buttonName={"LOGOUT"}/>
         </li>
       </ul>
