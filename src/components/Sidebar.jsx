d4cce4cbe8a6d156a48c7d415baf8c1984d0bc2d
tronross <john_ross@analogforever.ca>
2023-04-15 16:17:11 -0400

    Add conditional button styling to categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,24 +4,19 @@ export default function Sidebar(props) {
   const [clicked, setClicked] = useState({
     distance: 50,
     category: 'All Categories',
-    sort:     'Distance'
+    sort: 'Distance'
   });
 
   const dropdownSelect = (e) => {
     const select = e.target.value
-    // console.log(select)
     props.setCategory(select === 'All Categories' ? '' : select)
-    // console.log(clicked.category)
-    clicked.category = select;
-    // console.log(clicked.category)
+    setClicked(prev => ({ ...prev, category: select }));
     props.setFilters(prev => ({ ...prev, category: select }));
   }
 
   const closerThan = (e) => {
     const distance = e.target.value;
-    console.log(`Before: ${clicked.distance}`)
-    setClicked(prev => ({ ...prev, distance: distance}));
-    console.log(`After: ${clicked.distance}`)
+    setClicked(prev => ({ ...prev, distance: distance }));
     props.setFilters(prev => ({ ...prev, distance: distance }));
   }
 
@@ -33,7 +28,7 @@ export default function Sidebar(props) {
 
   const sortSelect = (e) => {
     const sort = e.target.value
-    clicked.sort = sort;
+    setClicked(prev => ({ ...prev, sort: sort }));
     props.setFilters(prev => ({ ...prev, sort: sort }));
   }
 
@@ -41,13 +36,18 @@ export default function Sidebar(props) {
     props.setFilters({
       distance: 50,
       category: 'All Categories',
-      sort:     'Distance',
-      date:     'All'
+      sort: 'Distance',
+      date: 'All'
     });
 
-    setClicked(prev => ({ ...prev, distance: 50}));
-    setClicked(prev => ({ ...prev, category: 'All Categories'}));
-    setClicked(prev => ({ ...prev, sort: 'Distance'}));
+    setClicked({
+      distance: 50,
+      category: 'All Categories',
+      sort: 'Distance',
+      date: 'All'
+    });
+    // setClicked(prev => ({ ...prev, category: 'All Categories' }));
+    // setClicked(prev => ({ ...prev, sort: 'Distance' }));
   }
 
 
@@ -59,47 +59,51 @@ export default function Sidebar(props) {
 
   return (
     <>
-    <div className="h-screen sticky top-0 ">
-      <section className="flex space-x-4" >
-        <h4 className= "m-4 text-lg text-teal-600 font-bold mb-2">Sort By:</h4>
-        <button type="button" className="inline-flex justify-center items-center gap-2 bg-teal-600 px-4 py-1 rounded text-white" value={'Distance'} onClick={sortSelect}>Distance</button>
-        <button className="inline-flex justify-center items-center gap-2 bg-teal-600 px-4 py-1 rounded text-white" value={'Date'} onClick={sortSelect}>Date</button>
-      </section>
-        <h4 className= "m-4 text-lg text-teal-600 font-bold mb-2">Filter By:</h4>
-      <section className="max-w-xs flex flex-col rounded-md shadow-sm space-y-0.5">
-        {props.sidebarOptions.map((item, idx) => {
-          return <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={idx} value={item} onClick={dropdownSelect}>{item}</button>
-        })}
+      <div className="h-screen sticky top-0 ">
+        <section className="flex space-x-4" >
+          <h4 className="m-4 text-lg text-teal-700 font-bold mb-2">Sort By:</h4>
+          <button type="button" className="inline-flex justify-center items-center gap-2 bg-teal-600 px-4 py-1 rounded text-white" value={'Distance'} onClick={sortSelect}>Distance</button>
+          <button className="inline-flex justify-center items-center gap-2 bg-teal-600 hover:bg-teal-700 active:bg-teal-800 px-4 py-1 rounded text-white" value={'Date'} onClick={sortSelect}>Date</button>
         </section>
-        <h5 className= "m-4 text-lg text-teal-700 font-bold mb-2">Distance (km):</h5>
+        <h4 className="m-4 text-lg text-teal-700 font-bold mb-2">Filter By:</h4>
+        <section className="max-w-xs flex flex-col rounded-md shadow-sm space-y-0.5">
+          {props.sidebarOptions.map((item, idx) => {
+            if (item === clicked.category) {
+              return <button className="w-full bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={idx} value={item} onClick={dropdownSelect}>{item}</button>
+            } else {
+              return <button className="w-full bg-teal-600 hover:bg-teal-700 active:bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={idx} value={item} onClick={dropdownSelect}>{item}</button>
+            }
+          })}
+        </section>
+        <h5 className="m-4 text-lg text-teal-700 font-bold mb-2">Distance (km):</h5>
         <section className="inline-flex rounded-md shadow-sm space-y-0.5">
-        {props.distances.map((distance, idx) => {
-          console.log(`distance: ${distance} >> clicked: ${clicked.distance}`)
-          if (parseInt(distance) == clicked.distance) {
-            return  <button type="button" className="w-full bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-m first:ml-0 last:rounded-r-m border" key={idx} value={distance} onClick={closerThan} >
-          {distance}</button>
-          } else {
-            return  <button type="button" className="w-full bg-teal-600 hover:bg-teal-700 active:bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-m first:ml-0 last:rounded-r-m border" key={idx} value={distance} onClick={closerThan} >
-            {distance}</button>
-          }
-        })}
-      </section>
-      <section className="max-w-xs flex flex-col rounded-md shadow-sm space-y-0.5">
-        <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={15} value={'All Categories'} onClick={resetFilters}>Reset Filters</button>
-      </section>
-    
+          {props.distances.map((distance, idx) => {
+            // Loose equality operator used to coerce type
+            if (parseInt(distance) == clicked.distance) {
+              return <button type="button" className="w-full bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-m first:ml-0 last:rounded-r-m border" key={idx} value={distance} onClick={closerThan} >
+                {distance}</button>
+            } else {
+              return <button type="button" className="w-full bg-teal-600 hover:bg-teal-700 active:bg-teal-800 px-4 py-1 rounded text-white inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-m first:ml-0 last:rounded-r-m border" key={idx} value={distance} onClick={closerThan} >
+                {distance}</button>
+            }
+          })}
+        </section>
+        <section className="max-w-xs flex flex-col rounded-md shadow-sm space-y-0.5">
+          <button className="w-full bg-teal-600 px-4 py-1 rounded text-white inline-flex justify-center shadow-sm" key={15} value={'All Categories'} onClick={resetFilters}>Reset Filters</button>
+        </section>
 
-      {/* <form>
+
+        {/* <form>
         <input type="date" name="selectedDate" onChange={selectDate} />
           <noscript>
             <input type="submit" value="Submit" />
           </noscript>
       </form> */}
-      {/* <input type="date" placeholder="Date"/> */}
-      {/* <input type="submit" value="Submit" /> */}
- 
-    
-    </div>
+        {/* <input type="date" placeholder="Date"/> */}
+        {/* <input type="submit" value="Submit" /> */}
+
+
+      </div>
     </>
   )
 }
