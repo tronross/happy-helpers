dd0e2766c57642c1b3471eb70eb437b496c0f56f
tronross <john_ross@analogforever.ca>
2023-04-16 10:33:37 -0400

    Add breakpoint before git stash pop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -98,7 +98,7 @@ export default function Sidebar(props) {
           <h5 className="m-4 text-lg text-teal-700 font-bold mb-2">City:</h5>
         <section className="flex flex-row space-x-1.5 items-baseline">
           <input className="h-8 w-400 block w-full text-m text-black file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 " name="cityFilter" type="text" onChange={filterCity} id="cityFilter" ></input>
-          <button className="inline-flex justify-center items-center gap-2 bg-teal-600 h-8 px-4 py-1 rounded text-white">clear</button>
+          <button className="inline-flex justify-center items-center gap-2 bg-teal-600 h-8 px-4 py-1 rounded text-white">reset</button>
         </section>
        
         <h5 className="m-4 text-lg text-teal-700 font-bold mb-2">Distance (km):</h5>
