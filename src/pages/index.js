ff6cc19b7e920056b0b2e7cce75bd2c54c10fd91
ACF <alricfernandes11@gmail.com>
2023-04-19 17:53:25 -0400

    Styling/formatting nav and first article

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import Image from 'next/image';
 
 // Component imports
 import Button from '@/components/Button';
 import Link from 'next/link';
 
-// import im from '../../../public/images/432.svg';
+// import main from 
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,56 +20,62 @@ export default function Landing() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <nav className="flex justify-between items-center p-5 text-teal-600 uppercase tracking-wide text-sm font-bold mb-2 fixed sticky top-0 z-50 bg-neutral-100">
-          <Link href="/home">
+      <main className="bg-teal-50">
+        <nav className="flex justify-between items-center p-5 tracking-wide font-bold mx-5 mb-2 fixed sticky top-0 z-50">
+          <Link className="text-green-900 text-xl" href="/home">
             Happy Helpers
           </Link>
           <Link href="/profile-page/1">
-            <button className="bg-teal-600 hover:bg-teal-700 active:bg-teal-800 uppercase font-bold px-3 py-1 rounded text-white h-8 text-sm">
-              LOGIN
+            <button className="bg-white hover:bg-teal-300 active:bg-green-900 px-10 py-1 rounded-full text-violet-400 h-8 text-xl">
+              Login
             </button>
           </Link>
         </nav>
-        <section className="flex justify-between">
-          <div>
-            <h1>Find your next volunteer Opportunity</h1>
-            <p>Happy Helpers gives everyone a chance to have a nearby hand when needed most.</p>
-          </div>
-          <img src="../../../public/images/432.svg" alt="432" />
-        </section>
+        <section className="mx-20">
+          <article className="flex justify-center">
+            <div className="w-1/2 flex flex-col justify-center">
+              <h1 className="text-green-900 font-bold text-4xl">
+                Find your next volunteer Opportunity
+              </h1>
+              <br></br>
+              <p className="text-xl text-green-900">
+                Happy Helpers gives everyone a chance to have a nearby hand when needed most.
+              </p>
+            </div>
+            <img src='/images/main.png' alt="main" className="w-1/2" />
+          </article>
 
-        <h1>How it Works?</h1>
+          {/* <h1>How it Works?</h1>
 
-        <section style={{ display: "flex" }}>
+        <article style={{ display: "flex" }}>
           <div>
             <p>Users can post a task they need help with or browse upcoming tasks which can be filtered by location, category and date.</p>
           </div>
           <img src='../../../public/images/13.svg' alt="13" />
-        </section>
+        </article>
 
-        <section style={{ display: "flex" }}>
+        <article style={{ display: "flex" }}>
           <img src='../../../public/images/168.svg' alt="168" />
           <div>
             <p>Offering help is simple, just find a task you want to volunteer for and hit OFFER. Once an offer comes in for one of your own tasks, you can view their profiles and select the best match.</p>
           </div>
-        </section>
+        </article>
 
-        <section style={{ display: "flex" }}>
+        <article style={{ display: "flex" }}>
           <div>
             <p>After being accepted for a task, you can view it on your profile page with more information. Patiently wait for the day to come to receive help or help your neighbor with their task.</p>
           </div>
           <img src='../../../public/images/51.svg' alt="51" />
-        </section>
+        </article>
 
-        <section style={{ display: "flex" }}>
+        <article style={{ display: "flex" }}>
           <img src='../../../public/images/93.svg' alt="93" />
           <div>
             <p>Once your task is finished don't forget to check it complete and reward your volunteer with a star if deserved. Stars show up on your profile to show others they can rely on you to get the task done.</p>
           </div>
-        </section>
+        </article>
 
-        <section>
+        <article>
           <h1>Sign Up and Join Our Community</h1>
           <div style={{ display: "flex" }}>
             <div>
@@ -82,8 +89,8 @@ export default function Landing() {
               <input type="text" id="password" name="password" placeholder="Password" />
             </form>
           </div>
+        </article> */}
         </section>
-
       </main>
     </>
   );
