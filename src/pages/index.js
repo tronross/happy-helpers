b3ea6688ebb201a7eef32f902bc7472e2cdaa9c3
ACF <alricfernandes11@gmail.com>
2023-04-20 17:57:00 -0400

    Changing background color

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,10 @@ export default function Landing() {
         <style>
           {
             `.bg-shape {
-                width: 620px;
-                height: 620px;
+                width: 420px;
+                height: 2620px;
                 border-radius: 9999px;
+                
             }
 
             .opacity-50 {
@@ -32,7 +33,7 @@ export default function Landing() {
             }
 
             .bg-blur {
-              filter: blur(150px)
+              filter: blur(300px)
             }
             `
           }
@@ -40,9 +41,10 @@ export default function Landing() {
       </Head>
       <main>
         <div className='absolute inset-0 flex justify-between'>
-          <div className='bg-shape bg-teal-600 opacity-50 bg-blur'></div>
-          <div className='bg-shape bg-violet-600 opacity-50 bg-blur'></div>
+          <div className='bg-shape bg-teal-300 opacity-50 bg-blur'></div>
+          <div className='bg-shape bg-violet-400 opacity-50 bg-blur'></div>
         </div>
+
         <nav className="flex justify-between items-center p-5 tracking-wide font-bold mx-5 mb-2 fixed sticky top-0 z-50">
           <Link className="text-green-900 text-xl" href="/">
             Happy Helpers
@@ -67,37 +69,37 @@ export default function Landing() {
             <img src="/images/main.png" alt="main" className="w-1/2" />
           </article>
 
-          <h1 className="flex justify-center text-green-900 font-bold text-4xl pt-40">How it Works?</h1>
+          <h1 className="flex justify-center text-green-900 font-bold text-4xl py-20">How it Works?</h1>
 
-          <article className="flex justify-center pt-20">
+          <article className="flex justify-center">
             <p className="w-1/2 flex flex-col justify-center text-3xl text-black-900">
               Users can post a task they need help with or browse upcoming tasks which can be filtered by location, category and date.
             </p>
             <img src="/images/1.png" alt="1" className="w-1/2" />
           </article>
 
-          <article className="flex justify-center pt-20">
+          <article className="flex justify-center">
             <img src='/images/2.png' alt="2" className="w-1/2" />
             <p className="flex flex-col justify-center text-3xl text-black-900 w-1/2">
               Offering help is simple, just find a task you want to volunteer for and hit OFFER. Once an offer comes in for one of your own tasks, you can view their profiles and select the best match.
             </p>
           </article>
 
-          <article className="flex justify-center pt-20">
+          <article className="flex justify-center">
             <p className="w-1/2 flex flex-col justify-center text-3xl text-black-900">
               After being accepted for a task, you can view it on your profile page with more information. Patiently wait for the day to come to receive help or help your neighbor with their task.
             </p>
             <img src="/images/3.png" alt="3" className="w-1/2" />
           </article>
 
-          <article className="flex justify-center pt-20">
+          <article className="flex justify-center pb-20">
             <img src="/images/4.png" alt="4" className="w-1/2" />
             <p className="flex flex-col justify-center text-3xl text-black-900 w-1/2">
               Once your task is finished don't forget to check it complete and reward your volunteer with a star if deserved. Stars show up on your profile to show others they can rely on you to get the task done.
             </p>
           </article>
 
-          <article className="border-black border-solid border-4 p-10 rounded-3xl">
+          <article className="border-violet border-4 p-10 rounded-3xl">
             <h1 className="flex justify-center text-green-900 font-bold text-4xl">
               Sign Up and Join Our Community
             </h1>
@@ -125,26 +127,26 @@ export default function Landing() {
           </article>
         </section>
       </main>
-      <footer className="flex bg-violet-400 mt-10 p-10 text-white">
-          <div className="pl-10 pr-40 border-solid border-white border-r-2">
-            <h1 className="font-bold text-2xl">Happy Helpers</h1>
-            <p className="text-m">Neighbors helping Neighbors</p>
-          </div>
-          <div className="pl-40">
-            <h1 className="font-bold text-2xl">About</h1>
-            <p>Careers</p>
-            <p>Our Projects</p>
-          </div>
-          <div className="pl-40">
-            <h1 className="font-bold text-2xl">Partnership</h1>
-            <p>Partners</p>
-            <p>Websites</p>
-          </div>
-          <div className="pl-40">
-            <h1 className="font-bold text-2xl">Support</h1>
-            <p>Support Request</p>
-            <p>Contact</p>
-          </div>
+      <footer className="flex bg-violet-400 mt-20 p-10 text-white">
+        <div className="pl-10 pr-40 border-solid border-white border-r-2">
+          <h1 className="font-bold text-2xl">Happy Helpers</h1>
+          <p className="text-m">Neighbors helping Neighbors</p>
+        </div>
+        <div className="pl-40">
+          <h1 className="font-bold text-2xl">About</h1>
+          <p>Careers</p>
+          <p>Our Projects</p>
+        </div>
+        <div className="pl-40">
+          <h1 className="font-bold text-2xl">Partnership</h1>
+          <p>Partners</p>
+          <p>Websites</p>
+        </div>
+        <div className="pl-40">
+          <h1 className="font-bold text-2xl">Support</h1>
+          <p>Support Request</p>
+          <p>Contact</p>
+        </div>
       </footer>
     </>
   );
