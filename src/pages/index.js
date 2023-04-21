1b295c671b937ed3c8482e9e65e2f0df284c1256
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-04-21 14:33:53 -0400

    Style nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,41 @@ import Image from 'next/image';
 // Component imports
 import Button from '@/components/Button';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Landing() {
 
+  const [clientWindowHeight, setClientWindowHeight] = useState();
+
+  const handleScroll = () => {
+    setClientWindowHeight(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    console.log(clientWindowHeight)
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  const [backgroundTransparacy, setBackgroundTransparacy] = useState(0);
+  const [padding, setPadding] = useState(30);
+  const [boxShadow, setBoxShadow] = useState(0);
+
+  useEffect(() => {
+    let backgroundTransparacyVar = clientWindowHeight / 600;
+
+    if (backgroundTransparacyVar < 1) {
+      let paddingVar = 30 - backgroundTransparacyVar * 20;
+      let boxShadowVar = backgroundTransparacyVar * 0.1;
+      setBackgroundTransparacy(backgroundTransparacyVar);
+      setPadding(paddingVar);
+      setBoxShadow(boxShadowVar);
+    }
+  }, [clientWindowHeight]);
+  
   // Template
   return (
     <>
@@ -37,18 +67,22 @@ export default function Landing() {
           }
         </style>
       </Head>
-      <main>
+      <main className='font-fredoka'>
         <div className='absolute inset-0 flex justify-between'>
           <div className='bg-shape bg-teal-300 opacity-50 bg-blur'></div>
           <div className='bg-shape bg-violet-400 opacity-50 bg-blur'></div>
         </div>
 
-        <nav className="flex justify-between items-center p-5 tracking-wide font-bold mx-5 mb-2 fixed sticky top-0 z-50">
-          <Link className="text-green-900 text-xl" href="/">
-            Happy Helpers
+        <nav className="flex justify-between items-center p-5 tracking-wide font-bold px-12 mb-2 fixed sticky top-0 z-50" style={{
+          background: `rgba(240, 253, 250, ${backgroundTransparacy})`,
+          // padding: `${padding}px 0px`,
+          boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`
+          }}>
+          <Link className="text-teal-700 text-2xl" href="/">
+            <p className='drop-shadow-md'>Happy Helpers</p>
           </Link>
           <Link href="/profile-page/1">
-            <button className="bg-white hover:bg-teal-300 active:bg-green-900 px-10 py-1 rounded-full text-violet-400 h-8 text-xl">
+            <button className="bg-violet-100 border-2 border-violet-300 hover:bg-violet-300 hover:text-white hover:border-violet-50 active:bg-violet-400 font-medium px-10 py-1 rounded-full text-violet-400 h-8 text-xl flex items-center justify-center drop-shadow-md">
               Login
             </button>
           </Link>
@@ -56,18 +90,18 @@ export default function Landing() {
         <section className="mx-20">
           <article className="flex justify-center">
             <div className="w-1/2 flex flex-col justify-center">
-              <h1 className="text-green-900 font-bold text-6xl">
+              <h1 className="text-teal-800 font-bold text-6xl">
                 Find your next volunteer Opportunity
               </h1>
               <br></br>
-              <p className="text-3xl text-green-900">
+              <p className="text-3xl text-teal-800">
                 Happy Helpers gives everyone a chance to have a nearby hand when needed most.
               </p>
             </div>
             <img src="/images/main.png" alt="main" className="w-1/2" />
           </article>
 
-          <h1 className="flex justify-center text-green-900 font-bold text-4xl py-20">How it Works?</h1>
+          <h1 className="flex justify-center text-teal-800 font-bold text-4xl py-20">How it Works?</h1>
 
           <article className="flex justify-center">
             <p className="w-1/2 flex flex-col justify-center text-3xl text-black-900">
@@ -98,27 +132,27 @@ export default function Landing() {
           </article>
 
           <article className="border-violet border-4 p-10 rounded-3xl">
-            <h1 className="flex justify-center text-green-900 font-bold text-4xl">
+            <h1 className="flex justify-center text-teal-800 font-bold text-4xl">
               Sign Up and Join Our Community
             </h1>
             <br></br>
             <div className="flex justify-center">
-              <div className="w-1/2 border-solid border-green-900 border-r-4 px-10">
-                <h1 className="text-green-900 text-5xl font-bold">
+              <div className="w-1/2 border-solid border-teal-800 border-r-4 px-10">
+                <h1 className="text-teal-800 text-5xl font-bold">
                   Happy Helpers
                 </h1>
                 <br></br>
                 <p className="text-black-900 text-2xl">A crowd-sourcing volunteering app with a focus on building community through in-person interactions and mutual aid.</p>
               </div>
               <form className="flex flex-col content-center w-1/2 px-10">
-                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-green-900 mb-5" type="text" id="firstName" name="firstName" placeholder="First Name" />
-                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-green-900 mb-5" type="text" id="lastName" name="lastName" placeholder="Last Name" />
-                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-green-900 mb-5" type="text" id="email" name="email" placeholder="Email" />
-                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-green-900 mb-5" type="text" id="password" name="password" placeholder="Password" />
+                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-teal-800 mb-5" type="text" id="firstName" name="firstName" placeholder="First Name" />
+                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-teal-800 mb-5" type="text" id="lastName" name="lastName" placeholder="Last Name" />
+                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-teal-800 mb-5" type="text" id="email" name="email" placeholder="Email" />
+                <input className="rounded-full border-violet-300 border-solid border-2 pl-10 text-2xl placeholder-teal-800 mb-5" type="text" id="password" name="password" placeholder="Password" />
               </form>
             </div>
             <div className="flex justify-center pt-10">
-              <button className="border-solid border-violet-300 border-2 bg-white hover:bg-violet-300 px-10 rounded-full text-green-900 h-8 text-xl">
+              <button className="border-solid border-violet-300 border-2 bg-white hover:bg-violet-300 px-10 rounded-full text-teal-800 h-8 text-xl">
                 Sign Up
               </button>
             </div>
